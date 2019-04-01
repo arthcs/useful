1ab71dca5fdced5934b9a2054d9a37a645c914fa
Arthur Cunha Silveira <arth.csil@gmail.com>
2019-04-01 15:55:14 -0300

    ini_set

---
+++ ini_set.php
@@ -0,0 +1,7 @@
+//error_reporting(0);
+set_time_limit(800000);
+ini_set('memory_limit','10240M');
+ini_set('upload_max_filesize','10000M');
+ini_set('post_max_size','10000M');
+ini_set('max_input_time',800000);
+ini_set('max_execution_time',800000);
