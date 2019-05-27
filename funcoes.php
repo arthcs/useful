8206cc457f307bd8d4c035a2bb66cdbf09209a71
Arthur Cunha Silveira <arth.csil@gmail.com>
2019-05-27 17:45:41 -0300

    Ultimas modificações

--- funcoes.php
+++ funcoes.php
@@ -20,6 +20,7 @@ Comando de comparação "like" no postgres não serve para inteiros.
   Nesse caso deve ser '=' no lugar de 'like', ficando: $where .= " id ='$this->idContrato'";
  O postgres é case sensitive para strings no banco de dados, por isso, tudo deve ser salvo em maiúsculo e pesquisado em maiúsculo.
   Exemplo: Verificar código fonte do model Contratos.php (tiss/sistema/model/Contratos.php).
+
  Listar não retorna nada devido ao problema de conversão para o php7 na hora de pegar o item do array.
   Exemplo: $row->$filtros[$i];
   Sugestão: 
@@ -39,3 +40,31 @@ Problema com as tabs, bug onde elas não são selecionas
 	ini_set('display_errors',1);
     ini_set('display_startup_erros',1);
     error_reporting(E_ALL);
+
+    //seleciona = "SELECT ifnull(guia_principal, id) as guia FROM tb_solicitacao_sadt WHERE id = '$id'";
+    $query=
+    $stm = $conn->prepare($query);
+	$stm->execute();
+	$dados = $stm->fetchObject();
+
+	//inserir ou Update
+	$insere = $conn->prepare("INSERT INTO tb_auxiliar_procedimentos (guia, coluna, status, informacoes_solicitadas, justificativa_negado, data_solicitacao, data_negado, quantidade_autorizada)
+										VALUES (:guia, :coluna, :status, :informacoes_solicitadas, :justificativa_negado, :data_solicitacao, :data_negado, :qtdAut)");
+			$insere->execute(array(
+				':guia'						=> $id,
+				':coluna'					=> $coluna,
+				':status'					=> $status,
+				':informacoes_solicitadas'	=> $infoComplIn,
+				':justificativa_negado'		=> $justificativaIn,
+				':data_solicitacao'			=> $dataSolIn,
+				':data_negado'				=> $dataNegadoIn,
+				':qtdAut'					=> $qtdAut
+			));
+			$linha = "status_procedimento_solicitado".$coluna;
+			$linhaQtd = "qntde_procedimento_autorizado".$coluna;
+
+			$altera = $conn->prepare("UPDATE tb_solicitacao_sadt SET ".$linha." = :valor, ".$linhaQtd." = :qtd WHERE id = '$id'");
+			$altera->execute(array(
+				':valor' => $status,
+				':qtd' 	 => $qtdAut
+			));
