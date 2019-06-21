45aea95afe4531262b8911c76a2d2a8ebbb30e3c
Arthur Cunha Silveira <arth.csil@gmail.com>
2019-06-21 17:44:04 -0300

    Mais funções

---
+++ functionJavaScript.php
@@ -0,0 +1,56 @@
+<script>
+    function numberToReal(numero) {
+        var numero = numero.toFixed(2).split('.');
+        numero[0] = numero[0].split(/(?=(?:...)*$)/).join('.');
+        return numero.join(',');
+    }
+
+	var x = numberToReal(9999000.33);
+	console.log(x);
+
+	var y = numberToReal(100000);
+	console.log(y);
+
+	var z = numberToReal(10.50);
+	console.log(z);
+	
+//melhor utilizada
+$(document).ready(function() {
+		$("input, select, textarea").attr("disabled", "true");
+
+		var numero = document.getElementById('valorProcedimento').value;
+		// console.log(numero);
+
+		numero = parseFloat(numero);
+        numero = numero.toFixed(2).split('.');
+        numero[0] = numero[0].split(/(?=(?:...)*$)/).join('.');
+
+        $('#valorProcedimento').val(numero.join(','));
+    });
+
+</script>
+
+Primeiro pego o número e fixo ele com 2 casas decimais e separo a string em um array de 2 partes (antes e depois do ponto).
+
+var numero = numero.toFixed(2).split('.');
+// Se o número inicial for 100.00, numero[0] será 100 e numero[1] será 00
+Dessa forma na segunda linha eu posso trabalhar o número excluindo as casas decimais (numero[0]), 
+
+numero[0] = "R$ " + numero[0].split(/(?=(?:...)*$)/).join('.');
+Depois é só retornar o número formatado juntando com as casas decimais usando a virgula.
+
+return numero.join(',');
+
+O que o split, o regex e o join fazem:
+Vamos por parte:
+
+/(?=(?:...)*$)/
+?= Vai capturar o espaço seguido da expressão após o =.
+?: Define toda a expressão dentro dos parênteses em um grupo de não captura.
+... Qualquer caractere 3 vezes.
+*$ Repetindo várias vezes no final da string.
+Explicando na prática:
+
+O que acontece é o seguinte, essa expressão agrupa 3 caracteres: (?:...), e faz a captura antes deles: ?=, o que garante que isso seja feito de trás pra frente infinitas vezes é o: *$, aplicando o split o número 1000000 ficaria dividido assim: 1|000|000, depois é só ele juntar com um ponto .join('.') que a mágica está feita.
+
+OBS.: O grupo de não captura ?: serve para não atrapalhar na hora de capturar o que realmente importa, que é antes dos 3 caracteres.
---
+++ styles.php
@@ -0,0 +1,16 @@
+  <style type="text/css">
+    .col-md-1, .col-md-2, .col-md-3, .col-md-4, .col-md-5, .col-md-6, .col-md-7, .col-md-8, .col-md-9, .col-md-10, .col-md-11, .col-md-12 {
+      height: 65px;
+      padding-left: 10px;
+      padding-right: 10px; 
+    }
+    div.no-display,
+    li.no-display,
+    .no-display {
+      display: none;
+    }
+    .no-bold {
+      font-weight: normal;
+      padding-right: 25px;
+    }
+  </style>
