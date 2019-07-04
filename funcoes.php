8cf44ebcf02eabef3a830005cda71da77024bc63
Arthur Cunha Silveira <arth.csil@gmail.com>
2019-07-04 10:57:23 -0300

    +funções para debug

--- funcoes.php
+++ funcoes.php
@@ -41,7 +41,10 @@ Problema com as tabs, bug onde elas não são selecionas
 		$(".dropdown-item").removeClass("active");
 	});
 
-	?>
+	
+
+	//desabilita os notice e o report de erros
+	error_reporting(0);
 
 	//codigo para debug
 	ini_set('display_errors',1);
@@ -75,3 +78,54 @@ Problema com as tabs, bug onde elas não são selecionas
 				':valor' => $status,
 				':qtd' 	 => $qtdAut
 			));
+?>
+
+<?php 
+//detecta encoding e altera.
+function detect_encoding($string)
+{
+    ////w3.org/International/questions/qa-forms-utf-8.html
+    if (preg_match('%^(?: [\x09\x0A\x0D\x20-\x7E] | [\xC2-\xDF][\x80-\xBF] | \xE0[\xA0-\xBF][\x80-\xBF] | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2} | \xED[\x80-\x9F][\x80-\xBF] | \xF0[\x90-\xBF][\x80-\xBF]{2} | [\xF1-\xF3][\x80-\xBF]{3} | \xF4[\x80-\x8F][\x80-\xBF]{2} )*$%xs', $string))
+        return 'UTF-8';
+ 
+    //If you need to distinguish between UTF-8 and ISO-8859-1 encoding, list UTF-8 first in your encoding_list.
+    //if you list ISO-8859-1 first, mb_detect_encoding() will always return ISO-8859-1.
+    return mb_detect_encoding($string, array('UTF-8', 'ASCII', 'ISO-8859-1', 'JIS', 'EUC-JP', 'SJIS'));
+}
+ 
+function convert_encoding($string, $to_encoding, $from_encoding = '')
+{
+    if ($from_encoding == '')
+        $from_encoding = detect_encoding($string);
+ 
+    if ($from_encoding == $to_encoding)
+        return $string;
+ 
+    return mb_convert_encoding($string, $to_encoding, $from_encoding);
+}
+//exemplo de uso: $newString = convert_encoding($oldString, 'UTF-8');
+
+
+//muda os caaracteres
+function charset_decode_utf_8 ($string) {
+        /* Only do the slow convert if there are 8-bit characters */
+        /* avoid using 0xA0 (\240) in ereg ranges. RH73 does not like that */
+        if (! preg_match("[\200-\237]", $string) and ! preg_match("[\241-\377]", $string))
+            return $string;
+ 
+        // decode three byte unicode characters
+        $string = preg_replace("/([\340-\357])([\200-\277])([\200-\277])/", 
+        "'&#'.((ord('\\1')-224)*4096 + (ord('\\2')-128)*64 + (ord('\\3')-128)).';'",   
+        $string);
+ 
+        // decode two byte unicode characters
+        $string = preg_replace("/([\300-\337])([\200-\277])/",
+        "'&#'.((ord('\\1')-192)*64+(ord('\\2')-128)).';'",
+        $string);
+ 
+        return $string;
+}
+//modo de usar
+$seuTextoTransformado = charset_decode_utf_8($textoATransformar);
+echo $seuTextoTransformado;
+ ?>
